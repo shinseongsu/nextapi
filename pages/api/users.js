dbae07006f8134e782e9d773abb28e1b2806b58a
Shin seongsu <tlstjdtn321@naver.com>
2022-06-29 02:35:04 +0900

    .

---
+++ pages/api/users.js
@@ -0,0 +1,34 @@
+// Fake users data
+import https from 'https';
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const { apiUrl } = req.body;
+
+  let httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+    cert: fs.readFileSync(
+      path.join(__dirname, '/ceriticates/certificate_sandbox.pem')
+    ),
+    key: fs.readFileSync(
+      path.join(__dirname, '/ceriticates/certificate_sandbox.key')
+    )
+  });
+
+  let response = axios.post(
+    apiUrl,
+    {
+      merchantIdentifier: 'merchant.kr.co.bluewalnut.CreamPayTest',
+      domainName: "tookmoe.bluewalnut.co.kr",
+      displayName: "bluewalnut.took"
+    },
+    {
+      httpsAgent
+    }
+  );
+
+
+  res.status(200).json(response)
+}
